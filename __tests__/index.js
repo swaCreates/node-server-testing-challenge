9a0ea6ceda9a192a905104e770f5d7a6dc2d2bca
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-05-15 17:59:00 -0400

    mvp completed

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,17 +1,23 @@
 const supertest= require('supertest');
 const server= require('../api/server.js');
 
-test('GET /', async () => {
-    // ARRANGE
-    const endpoint= '/';
-    const status= 200;
+describe('testing first endpoint', () => {
 
-    // ACT
-    const res= await supertest(server).get(endpoint);
-    console.log(res);
+    test('GET /', async () => {
+        // ARRANGE
+        const endpoint= '/';
+        const status= 200;
+    
+        // ACT
+        const res= await supertest(server).get(endpoint);
+        // this console.log allows us to see the different things
+        // we can test against
+        // console.log(res);
+    
+        // ASSERT
+        expect(res.statusCode).toBe(status);
+        expect(res.text).toBe(`<h4 align='center'>Welcome to my server! :)</h4>`)
+        expect(res.type).toBe('text/html'); // res.type is shorthand for res.headers["content-type"]
+    });
 
-    // ASSERT
-    expect(res.statusCode).toBe(status);
-    expect(res.text).toBe(`<h4 align='center'>Welcome to my server! :)</h4>`)
-    expect(res.type).toBe('text/html'); // res.type is shorthand for res.headers["content-type"]
 });
--- the_office/character-route.js
+++ the_office/character-route.js
@@ -3,35 +3,62 @@ const db= require('./character-module.js');
 
 const router= express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try {
-        
+        const chars= await db.fetch();
+        res.json(chars);
     } catch (err) {
-        
+        console.log('Fetch all:', err);
+        next(err);
     };
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
     try {
-        
+        const char= await db.fetchById(req.params.id);
+        if(!char){
+            return res.status(404).json({
+                message: 'Character was not found'
+            });
+        }
+        res.json(char);
     } catch (err) {
-        
+        console.log('Fetch by id:', err);
+        next(err);
     };
 });
 
 router.post('/', async (req, res, next) => {
     try {
-        
+        if(!req.body){
+            return res.status(400).json({
+                message: 'Must have a Character',
+            });
+        } else if(!req.body.name){
+            return res.status(400).json({
+                message: 'Must have a name',
+            });
+        } else if(!req.body.department){
+            return res.status(400).json({
+                message: 'Must have a department',
+            });
+        };
+        const [ id ]= await db.create(req.body);
+        const newChar= await db.fetchById(id);
+        res.status(201).json(newChar);
     } catch (err) {
-        
+        console.log('Create char:', err);
+        next(err);
     };
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
     try {
-        
+        await db.destroy(req.params.id);
+        res.status(204).end();
     } catch (err) {
-        
+        console.log('Deleted Char:', err);
+        next(err);
     };
 });
 
